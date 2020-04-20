d47d54cfa0a406655dc2ddc9d548c75828c6f0d2
Lyova Potyomkin <lyova.potyomkin@gmail.com>
2020-04-20 21:02:56 +0300

    Friendlier update interface

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -14,11 +14,16 @@ static const char *HELP = R"(COMMANDS:
     quit       - quit the client
 )";
 
-static std::string input(const std::string& prompt) {
+static std::string input(const std::string& prompt, 
+                         const std::string& def_value = "") {
     std::string value;
     std::cout << prompt;
+    if (def_value != "") {
+        std::cout << " [" << def_value << "]";
+    }
+    std::cout << ": ";
     std::getline(std::cin, value);
-    return value;
+    return value == "" ? def_value : value;
 }
 
 static void dump_entry(const Json::Value& entry) {
@@ -97,7 +102,7 @@ void Client::ping() {
 void Client::find() {
     Json::Value request;
     request["command"] = "find";
-    request["arg"] = std::stoi(input("ID (int): "));
+    request["arg"] = std::stoi(input("ID (int)"));
     Json::Value response = send_recv(request);
     int32_t status = response["status"].asInt();
     if (status == 404) {
@@ -111,7 +116,7 @@ void Client::find() {
 void Client::remove() {
     Json::Value request;
     request["command"] = "remove";
-    request["arg"] = std::stoi(input("ID (int): "));
+    request["arg"] = std::stoi(input("ID (int)"));
     Json::Value response = send_recv(request);
     int32_t status = response["status"].asInt();
     if (status == 200) {
@@ -124,9 +129,9 @@ void Client::remove() {
 void Client::add() {
     Json::Value request;
     request["command"] = "add";
-    request["arg"]["name"] = input("Name (string): ");
-    request["arg"]["price"] = std::stoi(input("Price (int): "));
-    request["arg"]["weight"] = std::stod(input("Weight (float): "));
+    request["arg"]["name"] = input("Name (string)");
+    request["arg"]["price"] = std::stoi(input("Price (int)"));
+    request["arg"]["weight"] = std::stod(input("Weight (float)"));
     Json::Value response = send_recv(request);
     int32_t status = response["status"].asInt();
     if (status == 200) {
@@ -136,25 +141,42 @@ void Client::add() {
 
 void Client::update() {
     Json::Value request;
-    request["command"] = "update";
-    request["arg"]["id"] = std::stoi(input("ID (int): "));
-    request["arg"]["name"] = input("New name (string): ");
-    request["arg"]["price"] = std::stoi(input("New price (int): "));
-    request["arg"]["weight"] = std::stod(input("New weight (float): "));
+    request["command"] = "find";
+    int32_t id = std::stoi(input("ID (int)"));
+    request["arg"] = id;
     Json::Value response = send_recv(request);
     int32_t status = response["status"].asInt();
-    if (status == 200) {
-        std::cout << "Successfully updated an entry\n";
-    } else if (status == 404) {
-        std::cout << "Requested entry was not found\n";
+    if (status == 404) {
+        std::cout << "No entry with given ID\n";
+        return;
+    } else if (status == 200) {
+        std::string old_name = response["result"]["name"].asString();
+        std::string old_price = response["result"]["price"].asString();
+        std::stringstream ss;
+        ss << std::fixed << std::setprecision(4) 
+           << response["result"]["weight"].asFloat();
+        std::string old_weight = ss.str();
+        request["command"] = "update";
+        request["arg"] = Json::objectValue;
+        request["arg"]["id"] = id;
+        request["arg"]["name"] = input("New name (string)", old_name);
+        request["arg"]["price"] = std::stoi(input("New price (int)", old_price));
+        request["arg"]["weight"] = std::stod(input("New weight (float)", old_weight));
+        Json::Value response = send_recv(request);
+        int32_t status = response["status"].asInt();
+        if (status == 200) {
+            std::cout << "Successfully updated an entry\n";
+        } else if (status == 404) {
+            std::cout << "Requested entry was not found\n";
+        }
     }
 }
 
 void Client::find_all() {
     Json::Value request;
     request["command"] = "find_all";
-    std::string field = input("Field to search by (name/price/weight): ");
-    std::string match = input("Value of the field: ");
+    std::string field = input("Field to search by (name/price/weight)");
+    std::string match = input("Value of the field");
     if (field == "name") {
         request["arg"][field] = match;
     } else if (field == "price") {
